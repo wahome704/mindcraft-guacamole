f2f0ac23cd6b0cf7a3827db07c869850f696b499
gabriel hope wahome <gabrielwahome22@gmail.com>
2026-09-20 15:30:05 +0300

    Add files via upload
    Initial commit of variables.c

---
+++ units offered.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	
+	printf("units offered This semester:\n");
+	printf("1.Introducion to programming \n");
+	printf("2.Data structures\n");
+	printf("3.computer Networks\n");
+	printf("4.Database Systems\n");
+	printf("5.Software Engineering\n");
+	
+	
+return 0;
+
+}
